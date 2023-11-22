65298b2f85566bbdc2af31f4b5b38326cfc3ba1b
jgwng <gunwng123@gmail.com>
2023-11-23 00:46:34 +0900

    axios 예제 추가

--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -3,9 +3,16 @@
     import { onMount } from 'svelte';
     let searchText = '';
     let searchWordList = [];
-
+    let IMG_END_POINT = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg';
+    
     onMount(() => {
-
+        let param = {};
+        param.word = '이상해씨';
+        param.snumber = 1;
+        param.snumber = 1010;
+        axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20').then(() => {
+            console.log('${result}')
+        });
     });
 
     function onKeyDown(event){
@@ -25,6 +32,8 @@
      on:keydown={onKeyDown} bind:value="{searchText}"/>
 
     {#each searchWordList as key}
-         <li>{key}</li>
+         <li>
+            <img src={IMG_END_POINT} alt='포켓몬 이미지' width="120" height="120"/>
+         </li>
     {/each}
 </div>
